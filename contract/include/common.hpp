fd07c6950107ac9defc2cfe870378258847e781c
changmyeong <changmyeong.baek@gmail.com>
2019-02-15 11:23:04 +0900

    add string replaceAll

--- contract/include/common.hpp
+++ contract/include/common.hpp
@@ -17,3 +17,17 @@ void split(vector<string>& result, const string& str, const string& delimiter)
         result.push_back(temp);
     }
 }
+
+string replaceAll(const string& base, const string& search, const string& change)
+{
+    size_t findIndex = -1;
+    size_t replaceSize = search.size();
+    string result = base;
+
+    while((findIndex = base.find(search, findIndex + 1)) != string::npos)
+    {
+        result.replace(findIndex, replaceSize, change.c_str());
+    }
+
+    return result;
+}
