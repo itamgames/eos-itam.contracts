841da6156d70c4bc60726c4979142b6403404226
changmyeong <changmyeong.baek@gmail.com>
2019-05-24 17:53:15 +0900

    delete no use actions, tables

--- contract/itamstoreran/itamstoreran.cpp
+++ contract/itamstoreran/itamstoreran.cpp
@@ -12,48 +12,31 @@ ACTION itamstoreran::registboard(string appId, string boardList)
 
     for(int i = 0; i < parsedBoardList.size(); i++)
     {
-        eosio_assert(isValidPrecision(parsedBoardList[i]["minimumScore"], parsedBoardList[i]["precision"]), "invalid precision of minimum score");
-        eosio_assert(isValidPrecision(parsedBoardList[i]["maximumScore"], parsedBoardList[i]["precision"]), "invalid precision of maximum score");
-
         boards.emplace(_self, [&](auto &b) {
             b.id = stoull(string(parsedBoardList[i]["id"]), 0, 10);
             b.name = parsedBoardList[i]["name"];
-            b.precision = parsedBoardList[i]["precision"];
-            b.minimumScore = parsedBoardList[i]["minimumScore"];
-            b.maximumScore = parsedBoardList[i]["maximumScore"];
         });
     }
 }
 
-ACTION itamstoreran::score(string appId, string boardId, string score, string owner, name ownerGroup, string nickname, string data)
+ACTION itamstoreran::score(string appId, string boardId, string score, name owner, name ownerGroup, string nickname, string data)
 {
+    require_auth(_self);
+    
     uint64_t appid = stoull(appId, 0, 10);
     uint64_t boardid = stoull(boardId, 0, 10);
-    require_auth(_self);
-    name groupAccount = get_group_account(owner, ownerGroup);
-    eosio_assert(is_account(groupAccount), "ownerGroup is not valid");
-    assertIfBlockUser(appid, owner, groupAccount);
+
+    name ownerAccount = get_group_account(owner, ownerGroup);
+    eosio_assert(is_account(ownerAccount), "ownerAccount is not valid");
 
     leaderboardTable boards(_self, appid);
     const auto& board = boards.get(boardid, "invalid board id");
-
-    eosio_assert(isValidPrecision(score, board.precision), "invalid precision of score");
-
-    asset scoreOfAdd;
-    stringToAsset(scoreOfAdd, score, board.precision);
-
-    asset minimumScore;
-    stringToAsset(minimumScore, board.minimumScore, board.precision);
-    eosio_assert(scoreOfAdd >= minimumScore, "score must be lower than minimum score");
-
-    asset maximumScore;
-    stringToAsset(maximumScore, board.maximumScore, board.precision);
-    eosio_assert(scoreOfAdd <= maximumScore, "score must be bigger than maximum score");
 }
 
 ACTION itamstoreran::rank(string appId, string boardId, string ranks, string period)
 {
     require_auth(_self);
+
     uint64_t appid = stoull(appId, 0, 10);
     uint64_t boardid = stoull(boardId, 0, 10);
     leaderboardTable(_self, appid).get(boardid, "invalid board id");
@@ -79,83 +62,32 @@ ACTION itamstoreran::regachieve(string appId, string achievementList)
     }
 }
 
-ACTION itamstoreran::acquisition(string appId, string achieveId, string owner, name ownerGroup, string data)
+ACTION itamstoreran::acquisition(string appId, string achieveId, name owner, name ownerGroup, string data)
 {
     require_auth(_self);
     
     uint64_t appid = stoull(appId, 0, 10);
     uint64_t achieveid = stoull(achieveId, 0, 10);
 
-    name groupAccount = get_group_account(owner, ownerGroup);
-    eosio_assert(is_account(groupAccount), "ownerGroup is not valid");
-    assertIfBlockUser(appid, owner, groupAccount);
+    name ownerAccount = get_group_account(owner, ownerGroup);
+    eosio_assert(is_account(ownerAccount), "ownerAccount is not valid");
 
     achievementTable achievements(_self, appid);
     achievements.get(achieveid, "invalid achieve id");
 }
 
-ACTION itamstoreran::cnlachieve(string appId, string achieveId, string owner, name ownerGroup, string reason)
+ACTION itamstoreran::cnlachieve(string appId, string achieveId, name owner, name ownerGroup, string reason)
 {
     require_auth(_self);
     uint64_t appid = stoull(appId, 0, 10);
     uint64_t achieveid = stoull(achieveId, 0, 10);
 
-    name groupAccount = get_group_account(owner, ownerGroup);
-    eosio_assert(is_account(groupAccount), "ownerGroup is not valid");
-    assertIfBlockUser(appid, owner, groupAccount);
+    name ownerAccount = get_group_account(owner, ownerGroup);
+    eosio_assert(is_account(ownerAccount), "ownerAccount is not valid");
     eosio_assert(reason.size() <= 256, "reason has more than 256 bytes");
 
     achievementTable achievements(_self, appid);
     achievements.get(achieveid, "invalid achieve id");
-}
-
-ACTION itamstoreran::blockuser(string appId, string owner, name ownerGroup, string reason)
-{
-    require_auth(_self);
-    name groupAccount = get_group_account(owner, ownerGroup);
-    eosio_assert(is_account(groupAccount), "ownerGroup is not valid");
-    eosio_assert(reason.size() <= 256, "reason has more than 256 bytes");
-
-    uint64_t appid = stoull(appId, 0, 10);
-    blockTable blocks(_self, appid);
-    const auto& block = blocks.find(groupAccount.value);
-
-    if(block == blocks.end())
-    {
-        blocks.emplace(_self, [&](auto &b) {
-            b.ownerGroup = groupAccount;
-            b.owners[owner] = blockInfo { now(), reason };
-        });
-    }
-    else
-    {
-        eosio_assert(block->owners.count(owner) == 0, "Already block user");
-        blocks.modify(block, _self, [&](auto &b) {
-            b.owners[owner] = blockInfo { now(), reason };
-        });
-    }
-}
-
-ACTION itamstoreran::unblockuser(string appId, string owner, name ownerGroup, string reason)
-{
-    require_auth(_self);
-    name groupAccount = get_group_account(owner, ownerGroup);    
-    eosio_assert(is_account(groupAccount), "ownerGroup is not valid");
-    eosio_assert(reason.size() <= 256, "reason has more than 256 bytes");
-
-    uint64_t appid = stoull(appId, 0, 10);
-    blockTable blocks(_self, appid);
-    const auto& block = blocks.require_find(groupAccount.value, "Already unblock");
-
-    eosio_assert(block->owners.count(owner) > 0, "Already unblock");
-    blocks.modify(block, _self, [&](auto &b) {
-        b.owners.erase(owner);
-    });
-
-    if(block->owners.size() == 0)
-    {
-        blocks.erase(block);
-    }
 }
 
 ACTION itamstoreran::delservice(string appId)
@@ -171,43 +103,9 @@ ACTION itamstoreran::delservice(string appId)
     for(auto iter = achievements.begin(); iter != achievements.end(); iter = achievements.erase(iter));
 }
 
-ACTION itamstoreran::history(string appId, string owner, name ownerGroup, string data)
+ACTION itamstoreran::history(string appId, name owner, name ownerGroup, string data)
 {
     require_auth(_self);
-    name groupAccount = get_group_account(owner, ownerGroup);
-    require_recipient(groupAccount);
-}
-
-void itamstoreran::assertIfBlockUser(uint64_t appId, const string& owner, name groupAccount)
-{
-    blockTable blocks(_self, appId);
-    const auto& block = blocks.find(groupAccount.value);
-    if(block == blocks.end())
-    {
-        return;
-    }
-
-    eosio_assert(block->owners.count(owner) == 0, "block user");
-}
-
-bool itamstoreran::isValidPrecision(const string& number, uint64_t precision)
-{
-    vector<string> score;
-
-    split(score, number, ".");
-
-    // score.size == 1 : decimal point size is 0
-    // score[1].size() <= precision : check decimal point of precision
-    return score.size() == 1 || score[1].size() <= precision;
-}
-
-void itamstoreran::stringToAsset(asset &result, const string& number, uint64_t precision)
-{
-    vector<string> amount;
-    split(amount, number, ".");
-
-    uint64_t decimalPointSize = amount.size() == 1 ? 0 : amount[1].size();
-
-    result.symbol = symbol("", precision);
-    result.amount = stoull(amount[0] + amount[1], 0, 10) * pow(10, precision - decimalPointSize);
+    name ownerAccount = get_group_account(owner, ownerGroup);
+    require_recipient(ownerAccount);
 }
--- contract/itamstoreran/itamstoreran.hpp
+++ contract/itamstoreran/itamstoreran.hpp
@@ -1,7 +1,6 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include "../include/json.hpp"
-#include "../include/string.hpp"
 #include "../include/dispatcher.hpp"
 #include "../include/ownergroup.hpp"
 
@@ -14,24 +13,15 @@ CONTRACT itamstoreran : public contract
     public:
         using contract::contract;
 
-        ACTION history(string appId, string owner, name ownerGroup, string data);
         ACTION delservice(string appId);
-
-        // leader board
         ACTION registboard(string appId, string boardList);
-        ACTION score(string appId, string boardId, string score, string owner, name ownerGroup, string nickname, string data);
+        ACTION score(string appId, string boardId, string score, name owner, name ownerGroup, string nickname, string data);
         ACTION rank(string appId, string boardId, string ranks, string period);
-
-        // achievements
         ACTION regachieve(string appId, string achievementList);
-        ACTION acquisition(string appId, string achieveId, string owner, name ownerGroup, string data);
-        ACTION cnlachieve(string appId, string achieveId, string owner, name ownerGroup, string reason);
-
-        // block
-        ACTION blockuser(string appId, string owner, name ownerGroup, string reason);
-        ACTION unblockuser(string appId, string owner, name ownerGroup, string reason);
+        ACTION acquisition(string appId, string achieveId, name owner, name ownerGroup, string data);
+        ACTION cnlachieve(string appId, string achieveId, name owner, name ownerGroup, string reason);
+        ACTION history(string appId, name owner, name ownerGroup, string data);
     private:
-        // leader board
         TABLE leaderboard
         {
             uint64_t id;
@@ -52,26 +42,6 @@ CONTRACT itamstoreran : public contract
             uint64_t primary_key() const { return id; }
         };
         typedef multi_index<name("achievements"), achievement> achievementTable;
-
-        struct blockInfo
-        {
-            uint64_t timestamp;
-            string reason;
-        };
-
-        TABLE block
-        {
-            name ownerGroup;
-            map<string, blockInfo> owners;
-
-            uint64_t primary_key() const { return ownerGroup.value; }
-        };
-        typedef multi_index<name("blocks"), block> blockTable;
-
-    private:
-        void assertIfBlockUser(uint64_t appId, const string& owner, name groupAccount);
-        void stringToAsset(asset &result, const string& number, uint64_t precision);
-        bool isValidPrecision(const string& number, uint64_t precision);
 };
 
-EOSIO_DISPATCH_EX( itamstoreran, (registboard)(score)(rank)(regachieve)(acquisition)(cnlachieve)(blockuser)(unblockuser)(delservice)(history) )
+EOSIO_DISPATCH_EX( itamstoreran, (registboard)(score)(rank)(regachieve)(acquisition)(cnlachieve)(delservice)(history) )
