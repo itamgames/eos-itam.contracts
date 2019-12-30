824449b88e467d46101bf518ed8e27b7c90b7b61
changmyeong <changmyeong.baek@gmail.com>
2019-12-30 15:44:24 +0900

    set SECONDS_OF_DAY value on build time

--- contract/include/date.hpp
+++ contract/include/date.hpp
@@ -1,2 +1,6 @@
 // 1 day == 24 hours == 1440 minutes == 86400 seconds
-const int SECONDS_OF_DAY = 86400;
+#ifdef testnet
+    const int SECONDS_OF_DAY = 60;
+#else
+    const int SECONDS_OF_DAY = 86400;
+#endif
