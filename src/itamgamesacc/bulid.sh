2726c036251fb6983a83cd8b34b6a424a809a9be
changmyeong <changmyeong.baek@gmail.com>
2020-04-27 17:42:26 +0900

    add common integrate contract

---
+++ src/itamgamesacc/bulid.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+COMPILE_OPTION=""
+if [ "$1" = "mainnet" ]; then
+    COMPILE_OPTION="MAINNET"
+elif [ "$1" = "mainnet-beta" ]; then
+    COMPILE_OPTION="MAINNET_BETA"
+elif [ "$1" = "testnet" ]; then
+    COMPILE_OPTION="TESTNET"
+elif [ "$1" = "testnet-beta" ]; then
+    COMPILE_OPTION="TESTNET_BETA"
+else
+    echo "require option: \"mainnet\", \"mainnet-beta\", \"testnet\", \"testnet-beta\""
+    exit 1
+fi
+
+eosio-cpp -abigen -D="$COMPILE_OPTION" itamgamesacc.cpp -o itamgamesacc.wasm
---
+++ src/itamgamesacc/itamgamesacc.hpp
@@ -0,0 +1,22 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include "../include/dispatcher.hpp"
+
+using namespace eosio;
+
+CONTRACT itamgamesacc : contract
+{
+    public:
+        using contract::contract;
+
+        ACTION achievement(uint64_t appId, uint64_t achievementId, string user, string data, uint64_t timestamp);
+        ACTION leaderboard(uint64_t appId, uint64_t leaderboardId, string user, string data, uint64_t timestamp);
+        ACTION history(uint64_t appId, string user, string data, uint64_t timestamp);
+        ACTION nft(uint64_t appId, uint64_t nftId, string data, string state, uint64_t timestamp);
+        ACTION createorder(uint64_t appId, uint64_t nftId, string user, string price, uint64_t timestamp);
+        ACTION modifyorder(uint64_t appId, uint64_t nftId, string price, uint64_t timestamp)
+        ACTION deleteorder(uint64_t appId, uint64_t nftId, string user, uint64_t timestamp);
+        ACTION order(uint64_t appId, uint64_t nftId, string from, string to, uint64_t timestamp);
+};
+
+ALLOW_TRANSFER_ALL_DISPATCHER( itamgamesacc, (achievement)(leaderboard)(history)(nft)(createorder)(modifyorder)(deleteorder)(order) )
