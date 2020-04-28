fe58d7f59fa6f8aeddfd0235798309e37a52595d
changmyeong <changmyeong.baek@gmail.com>
2020-04-28 16:29:17 +0900

    build

--- src/itamgamesacc/itamgamesacc.hpp
+++ src/itamgamesacc/itamgamesacc.hpp
@@ -3,6 +3,7 @@
 #include "../include/dispatcher.hpp"
 
 using namespace eosio;
+using namespace std;
 
 CONTRACT itamgamesacc : contract
 {
@@ -14,7 +15,7 @@ CONTRACT itamgamesacc : contract
         ACTION history(uint64_t appId, string user, string data, uint64_t timestamp);
         ACTION nft(uint64_t appId, uint64_t nftId, string data, string state, uint64_t timestamp);
         ACTION createorder(uint64_t appId, uint64_t nftId, string user, string price, uint64_t timestamp);
-        ACTION modifyorder(uint64_t appId, uint64_t nftId, string price, uint64_t timestamp)
+        ACTION modifyorder(uint64_t appId, uint64_t nftId, string price, uint64_t timestamp);
         ACTION deleteorder(uint64_t appId, uint64_t nftId, string user, uint64_t timestamp);
         ACTION order(uint64_t appId, uint64_t nftId, string from, string to, uint64_t timestamp);
 };
