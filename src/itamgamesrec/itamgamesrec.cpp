1b62c29245b88cad8c5d32ee6f89bc86e27d7059
changmyeong <changmyeong.baek@gmail.com>
2020-06-28 02:14:41 +0900

    change nonce type, add price in deleteorder action

--- src/itamgamesrec/itamgamesrec.cpp
+++ src/itamgamesrec/itamgamesrec.cpp
@@ -1,51 +1,51 @@
 #include "itamgamesrec.hpp"
 
-ACTION itamgamesrec::achievement(uint64_t appId, uint64_t achievementId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::achievement(uint64_t appId, uint64_t achievementId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::leaderboard(uint64_t appId, uint64_t leaderboardId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::leaderboard(uint64_t appId, uint64_t leaderboardId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::gamehistory(uint64_t appId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::gamehistory(uint64_t appId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::createnft(uint64_t appId, uint64_t nftId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::createnft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::modifynft(uint64_t appId, uint64_t nftId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::modifynft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::deletenft(uint64_t appId, uint64_t nftId, string gameAccount, string json, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::deletenft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::createorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::createorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::modifyorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::modifyorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::deleteorder(uint64_t appId, uint64_t nftId, string gameAccount, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::deleteorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
 
-ACTION itamgamesrec::order(uint64_t appId, uint64_t nftId, string from, string to, string price, uint64_t nonce, uint64_t timestamp)
+ACTION itamgamesrec::order(uint64_t appId, uint64_t nftId, string from, string to, string price, string nonce, uint64_t timestamp)
 {
     require_auth(_self);
 }
