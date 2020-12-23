f07598e288f2f092f7a6c6b04ff8aa9004db4846
changmyeong <changmyeong.baek@gmail.com>
2020-12-23 13:53:16 +0900

    use uuid

--- src/itamgamesrec/itamgamesrec.hpp
+++ src/itamgamesrec/itamgamesrec.hpp
@@ -13,13 +13,14 @@ CONTRACT itamgamesrec : contract
         ACTION achievement(uint64_t appId, uint64_t achievementId, string gameAccount, string data, string nonce, uint64_t timestamp);
         ACTION leaderboard(uint64_t appId, uint64_t leaderboardId, string gameAccount, string json, string nonce, uint64_t timestamp);
         ACTION gamehistory(uint64_t appId, string gameAccount, string json, string nonce, uint64_t timestamp);
-        ACTION createnft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp);
-        ACTION modifynft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp);
-        ACTION deletenft(uint64_t appId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp);
-        ACTION createorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp);
-        ACTION modifyorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp);
-        ACTION deleteorder(uint64_t appId, uint64_t nftId, string gameAccount, string price, string nonce, uint64_t timestamp);
-        ACTION order(uint64_t appId, uint64_t nftId, string from, string to, string price, string nonce, uint64_t timestamp);
+        ACTION digitalasset(uint64_t appId, uint64_t digitalAssetId, string name, string description, string image);
+        ACTION createnft(string uuid, uint64_t appId, uint64_t digitalAssetId, string gameAccount, string json, string nonce, uint64_t timestamp);
+        ACTION modifynft(string uuid, uint64_t appId, uint64_t digitalAssetId, string gameAccount, string json, string nonce, uint64_t timestamp);
+        ACTION deletenft(string uuid, uint64_t appId, uint64_t digitalAssetId, uint64_t nftId, string gameAccount, string json, string nonce, uint64_t timestamp);
+        ACTION createorder(string uuid, uint64_t appId, uint64_t digitalAssetId, string gameAccount, string price, string nonce, uint64_t timestamp);
+        ACTION modifyorder(string uuid, uint64_t appId, uint64_t digitalAssetId, string gameAccount, string price, string nonce, uint64_t timestamp);
+        ACTION deleteorder(string uuid, uint64_t appId, uint64_t digitalAssetId, string gameAccount, string price, string nonce, uint64_t timestamp);
+        ACTION order(string uuid, uint64_t appId, uint64_t digitalAssetId, string from, string to, string price, string nonce, uint64_t timestamp);
 };
 
 ALLOW_TRANSFER_ALL_DISPATCHER( itamgamesrec, (achievement)(leaderboard)(gamehistory)(createnft)(modifynft)(deletenft)(createorder)(modifyorder)(deleteorder)(order) )
